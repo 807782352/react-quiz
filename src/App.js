9e2ab29817ae38efc332857bc6d9828c39e62e8f
z297xu <kyrie807782352@gmail.com>
2023-11-20 16:53:49 -0500

    displaying questions

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ const initialState = {
 
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
+
+  // current question idx
+  index: 0,
+
 };
 
 function reducer(state, action) {
@@ -38,7 +42,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
 
@@ -57,7 +61,7 @@ export default function App() {
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
         {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]}/>}
       </Main>
     </div>
   );
