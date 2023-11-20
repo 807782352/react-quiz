cd9449c84c377372bdabec394f00397963630cb7
z297xu <kyrie807782352@gmail.com>
2023-11-20 16:47:10 -0500

    start a quick quiz

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./components/Main";
 import Loader from "./components/Loader";
 import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
 
 const initialState = {
   questions: [],
@@ -25,6 +26,11 @@ function reducer(state, action) {
         ...state,
         status: "error",
       };
+    case "start":
+      return {
+        ...state,
+        status: "active"
+      }
 
     default:
       throw new Error("Action unknown");
@@ -50,7 +56,8 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
